f8421299c8fc99a95e94f51e8b507f94d3713dd5
dongxingpeng <291785613@qq.com>
2020-08-19 10:18:20 +0800

    补充

--- public/env.config.js
+++ public/env.config.js
@@ -1,19 +1,19 @@
 /*
  接口外置，进行自行配置
- host可以改成任意地址:http://www.espweb.com:31138
+ host可以改成任意地址
  */
 var host = window.location.protocol + "//" + window.location.host
 window.config = {
   // 接口请求
   VUE_APP_API: host + "",
-  // 图片预览地址
+  // 图片拼接预览地址
   VUE_APP_IMG_ICON: host + "",
-  // 图片上传地址
+  // 文件上传地址
   VUE_APP_IMG_UPLOAD: host + "",
-  // 附件下载
+  // 下载
   VUE_APP_FILE_DOWNLOAD: host + "",
-  // 重新登录
+  // 登录
   VUE_APP_LOGIN: host + "",
-  // 进入门户网站首页
+  // 进入首页
   VUE_APP_INDEX: host + "",
 }
